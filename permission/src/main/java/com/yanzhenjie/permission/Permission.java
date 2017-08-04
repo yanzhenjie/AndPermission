dc4d106d23d4568cf8b62fa22edb4d1e51cde206
YanZhenjie <smallajax@foxmail.com>
2017-08-04 13:42:54 +0800

    Add request permission group.

---
+++ permission/src/main/java/com/yanzhenjie/permission/Permission.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright © Yan Zhenjie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.yanzhenjie.permission;
+
+import android.Manifest;
+import android.os.Build;
+
+/**
+ * <p>Permission Group.</p>
+ * Created by YanZhenjie on 2017/8/4.
+ */
+public final class Permission {
+
+    public static final String[] CALENDAR;
+    public static final String[] CAMERA;
+    public static final String[] CONTACTS;
+    public static final String[] LOCATION;
+    public static final String[] MICROPHONE;
+    public static final String[] PHONE;
+    public static final String[] SENSORS;
+    public static final String[] SMS;
+    public static final String[] STORAGE;
+
+    static {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            CALENDAR = new String[]{};
+            CAMERA = new String[]{};
+            CONTACTS = new String[]{};
+            LOCATION = new String[]{};
+            MICROPHONE = new String[]{};
+            PHONE = new String[]{};
+            SENSORS = new String[]{};
+            SMS = new String[]{};
+            STORAGE = new String[]{};
+        } else {
+            CALENDAR = new String[]{
+                    Manifest.permission.READ_CALENDAR,
+                    Manifest.permission.WRITE_CALENDAR};
+
+            CAMERA = new String[]{
+                    Manifest.permission.CAMERA};
+
+            CONTACTS = new String[]{
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.WRITE_CONTACTS,
+                    Manifest.permission.GET_ACCOUNTS};
+
+            LOCATION = new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION};
+
+            MICROPHONE = new String[]{
+                    Manifest.permission.RECORD_AUDIO};
+
+            PHONE = new String[]{
+                    Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.CALL_PHONE,
+                    Manifest.permission.READ_CALL_LOG,
+                    Manifest.permission.WRITE_CALL_LOG,
+                    Manifest.permission.ADD_VOICEMAIL,
+                    Manifest.permission.USE_SIP,
+                    Manifest.permission.PROCESS_OUTGOING_CALLS};
+
+            SENSORS = new String[]{
+                    Manifest.permission.BODY_SENSORS};
+
+            SMS = new String[]{
+                    Manifest.permission.SEND_SMS,
+                    Manifest.permission.RECEIVE_SMS,
+                    Manifest.permission.READ_SMS,
+                    Manifest.permission.RECEIVE_WAP_PUSH,
+                    Manifest.permission.RECEIVE_MMS};
+
+            STORAGE = new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        }
+    }
+
+}
