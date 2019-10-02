6de8648f1c720f5acd23d6de7d0b66078ec843ef
Zhenjie Yan <im.yanzhenjie@gmail.com>
2019-10-02 10:38:16 +0800

    Test write contact, close #431.

--- permission/src/main/java/com/yanzhenjie/permission/checker/ContactsWriteTest.java
+++ permission/src/main/java/com/yanzhenjie/permission/checker/ContactsWriteTest.java
@@ -22,6 +22,8 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
 
+import androidx.annotation.Nullable;
+
 /**
  * Created by Zhenjie Yan on 2018/1/14.
  */
@@ -37,24 +39,16 @@ class ContactsWriteTest implements PermissionTest {
 
     @Override
     public boolean test() throws Throwable {
-        Cursor cursor = mResolver.query(ContactsContract.Data.CONTENT_URI,
-            new String[] {ContactsContract.Data.RAW_CONTACT_ID},
-            ContactsContract.Data.MIMETYPE + "=? and " + ContactsContract.Data.DATA1 + "=?",
-            new String[] {ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, DISPLAY_NAME}, null);
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                long rawContactId = cursor.getLong(0);
-                cursor.close();
-                return update(rawContactId);
-            } else {
-                cursor.close();
-                return insert();
-            }
+        long[] idArray = insert();
+        long rawContactId = idArray[0];
+        long dataId = idArray[1];
+        if (rawContactId > 0 && dataId > 0) {
+            return delete(rawContactId, dataId);
         }
         return false;
     }
 
-    private boolean insert() {
+    private long[] insert() {
         ContentValues values = new ContentValues();
         Uri rawContractUri = mResolver.insert(ContactsContract.RawContacts.CONTENT_URI, values);
         long rawContactId = ContentUris.parseId(rawContractUri);
@@ -64,14 +58,16 @@ class ContactsWriteTest implements PermissionTest {
         values.put(ContactsContract.Data.DATA2, DISPLAY_NAME);
         values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
         Uri dataUri = mResolver.insert(ContactsContract.Data.CONTENT_URI, values);
-        return ContentUris.parseId(dataUri) > 0;
+        long dataId = ContentUris.parseId(dataUri);
+        return new long[]{rawContactId, dataId};
     }
 
-    private void delete(long contactId, long dataId) {
-        mResolver.delete(ContactsContract.RawContacts.CONTENT_URI, ContactsContract.RawContacts._ID + "=?",
-            new String[] {Long.toString(contactId)});
-        mResolver.delete(ContactsContract.Data.CONTENT_URI, ContactsContract.Data._ID + "=?",
-            new String[] {Long.toString(dataId)});
+    private boolean delete(long rawContactId, long dataId) {
+        int dataCount = mResolver.delete(ContactsContract.Data.CONTENT_URI, ContactsContract.Data._ID + "=?",
+            new String[]{Long.toString(dataId)});
+        int rawContactCount = mResolver.delete(ContactsContract.RawContacts.CONTENT_URI, ContactsContract.RawContacts._ID + "=?",
+            new String[]{Long.toString(rawContactId)});
+        return rawContactCount > 0 && dataCount > 0;
     }
 
     private boolean update(long rawContactId) {
@@ -82,5 +78,21 @@ class ContactsWriteTest implements PermissionTest {
         values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
         Uri dataUri = mResolver.insert(ContactsContract.Data.CONTENT_URI, values);
         return ContentUris.parseId(dataUri) > 0;
+    }
+
+    @Nullable
+    private long[] query() {
+        Cursor cursor = mResolver.query(ContactsContract.Data.CONTENT_URI,
+            new String[]{ContactsContract.Data.RAW_CONTACT_ID, ContactsContract.Data._ID},
+            ContactsContract.Data.MIMETYPE + "=? and " + ContactsContract.Data.DATA1 + "=?",
+            new String[]{ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, DISPLAY_NAME}, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                long rawContactId = cursor.getLong(cursor.getColumnIndex(ContactsContract.Data.RAW_CONTACT_ID));
+                long dataId = cursor.getLong(cursor.getColumnIndex(ContactsContract.Data._ID));
+                return new long[]{rawContactId, dataId};
+            }
+        }
+        return null;
     }
 }
