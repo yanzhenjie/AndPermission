6d37da710a29639afe152654a636d7dad02c9ad1
Zhenjie Yan <im.yanzhenjie@gmail.com>
2019-10-03 21:07:20 +0800

    Get the device status for Android Q, #478.

--- permission/src/main/java/com/yanzhenjie/permission/checker/PhoneStateReadTest.java
+++ permission/src/main/java/com/yanzhenjie/permission/checker/PhoneStateReadTest.java
@@ -17,8 +17,8 @@ package com.yanzhenjie.permission.checker;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.telephony.TelephonyManager;
-import android.text.TextUtils;
 
 /**
  * Created by Zhenjie Yan on 2018/1/25.
@@ -36,9 +36,9 @@ class PhoneStateReadTest implements PermissionTest {
         PackageManager packageManager = mContext.getPackageManager();
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) return true;
 
-        TelephonyManager telephonyManager = (TelephonyManager)mContext.getSystemService(Context.TELEPHONY_SERVICE);
-        return telephonyManager.getPhoneType() == TelephonyManager.PHONE_TYPE_NONE ||
-            !TextUtils.isEmpty(telephonyManager.getDeviceId()) ||
-            !TextUtils.isEmpty(telephonyManager.getSubscriberId());
+        TelephonyManager telephonyManager = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) telephonyManager.getDeviceId();
+        else telephonyManager.getDeviceSoftwareVersion();
+        return true;
     }
 }
