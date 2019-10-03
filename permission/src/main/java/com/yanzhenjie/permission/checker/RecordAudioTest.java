ddcbc4c90b827cfa2ea98fbbc887093618447b78
Zhenjie Yan <im.yanzhenjie@gmail.com>
2019-10-03 17:12:20 +0800

    Accurately verify AudioRecord permission, close #483.

--- permission/src/main/java/com/yanzhenjie/permission/checker/RecordAudioTest.java
+++ permission/src/main/java/com/yanzhenjie/permission/checker/RecordAudioTest.java
@@ -21,12 +21,16 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by Zhenjie Yan on 2018/1/14.
  */
 class RecordAudioTest implements PermissionTest {
 
-    private static final int[] RATES = new int[] {8000, 11025, 22050, 44100};
+    private static final int[] RATES = new int[]{8000, 11025, 22050, 44100};
 
     private Context mContext;
 
@@ -36,18 +40,48 @@ class RecordAudioTest implements PermissionTest {
 
     @Override
     public boolean test() throws Throwable {
-        AudioRecord audioRecord = findAudioRecord();
+        AudioRecord audioRecord = null;
+        File file = null;
+        FileOutputStream fos = null;
         try {
-            if (audioRecord != null) {
-                audioRecord.startRecording();
-            } else {
-                return !existMicrophone(mContext);
-            }
+            int[] params = findAudioParameters();
+            if (params == null) return !existMicrophone(mContext);
+
+            audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, params[0], params[1], params[2], params[3]);
+            int state = audioRecord.getState();
+            if (state != AudioRecord.STATE_INITIALIZED) return !existMicrophone(mContext);
+
+            int recordState = audioRecord.getRecordingState();
+            if (recordState != AudioRecord.RECORDSTATE_STOPPED) return true;
+
+            audioRecord.startRecording();
+            if (audioRecord.getRecordingState() != AudioRecord.RECORDSTATE_RECORDING) return true;
+
+            File cacheDir = new File(mContext.getCacheDir(), "_andpermission_audio_record_test_");
+            cacheDir.mkdirs();
+
+            file = new File(cacheDir, Long.toString(System.currentTimeMillis()));
+            if (file.exists()) file.createNewFile();
+
+            fos = new FileOutputStream(file);
+            byte[] buffer = new byte[params[3]];
+
+            int len = audioRecord.read(buffer, 0, params[3]);
+            fos.write(buffer, 0, len);
+            fos.flush();
         } catch (Throwable e) {
             return !existMicrophone(mContext);
         } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException ignored) {
+                }
+            }
+            if (file != null && file.exists()) {
+                file.delete();
+            }
             if (audioRecord != null) {
-                audioRecord.stop();
                 audioRecord.release();
             }
         }
@@ -59,15 +93,13 @@ class RecordAudioTest implements PermissionTest {
         return packageManager.hasSystemFeature(PackageManager.FEATURE_MICROPHONE);
     }
 
-    private static AudioRecord findAudioRecord() {
+    private static int[] findAudioParameters() {
         for (int rate : RATES) {
-            for (short format : new short[] {AudioFormat.ENCODING_PCM_8BIT, AudioFormat.ENCODING_PCM_16BIT}) {
-                for (short channel : new short[] {AudioFormat.CHANNEL_IN_MONO, AudioFormat.CHANNEL_IN_STEREO}) {
+            for (int channel : new int[]{AudioFormat.CHANNEL_IN_MONO, AudioFormat.CHANNEL_IN_STEREO}) {
+                for (int format : new int[]{AudioFormat.ENCODING_PCM_8BIT, AudioFormat.ENCODING_PCM_16BIT}) {
                     int buffer = AudioRecord.getMinBufferSize(rate, channel, format);
                     if (buffer != AudioRecord.ERROR_BAD_VALUE) {
-                        AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, rate, channel, format,
-                            buffer);
-                        if (recorder.getState() == AudioRecord.STATE_INITIALIZED) return recorder;
+                        return new int[]{rate, channel, format, buffer};
                     }
                 }
             }
