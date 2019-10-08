c51164101f9584d2ec5f07d88f3bd22a11004137
Zhenjie Yan <im.yanzhenjie@gmail.com>
2019-10-08 14:47:30 +0800

    Support ACCESS_BACKGROUND_LOCATION, #478.

--- permission/src/main/java/com/yanzhenjie/permission/runtime/Permission.java
+++ permission/src/main/java/com/yanzhenjie/permission/runtime/Permission.java
@@ -41,6 +41,7 @@ public class Permission {
 
     public static final String ACCESS_FINE_LOCATION = "android.permission.ACCESS_FINE_LOCATION";
     public static final String ACCESS_COARSE_LOCATION = "android.permission.ACCESS_COARSE_LOCATION";
+    public static final String ACCESS_BACKGROUND_LOCATION = "android.permission.ACCESS_BACKGROUND_LOCATION";
 
     public static final String RECORD_AUDIO = "android.permission.RECORD_AUDIO";
 
@@ -68,38 +69,48 @@ public class Permission {
 
     public static final class Group {
 
-        public static final String[] CALENDAR = new String[] {Permission.READ_CALENDAR, Permission.WRITE_CALENDAR};
+        public static final String[] CALENDAR = new String[]{Permission.READ_CALENDAR, Permission.WRITE_CALENDAR};
 
-        public static final String[] CAMERA = new String[] {Permission.CAMERA};
+        public static final String[] CAMERA = new String[]{Permission.CAMERA};
 
-        public static final String[] CONTACTS = new String[] {Permission.READ_CONTACTS, Permission.WRITE_CONTACTS,
+        public static final String[] CONTACTS = new String[]{Permission.READ_CONTACTS, Permission.WRITE_CONTACTS,
             Permission.GET_ACCOUNTS};
 
-        public static final String[] LOCATION = new String[] {Permission.ACCESS_FINE_LOCATION,
-            Permission.ACCESS_COARSE_LOCATION};
+        public static final String[] LOCATION;
 
-        public static final String[] MICROPHONE = new String[] {Permission.RECORD_AUDIO};
+        static {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                LOCATION = new String[]{Permission.ACCESS_FINE_LOCATION,
+                    Permission.ACCESS_COARSE_LOCATION,
+                    Permission.ACCESS_BACKGROUND_LOCATION};
+            } else {
+                LOCATION = new String[]{Permission.ACCESS_FINE_LOCATION,
+                    Permission.ACCESS_COARSE_LOCATION};
+            }
+        }
+
+        public static final String[] MICROPHONE = new String[]{Permission.RECORD_AUDIO};
 
         public static final String[] PHONE;
 
         static {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                PHONE = new String[] {Permission.READ_PHONE_STATE, Permission.CALL_PHONE, Permission.READ_CALL_LOG,
+                PHONE = new String[]{Permission.READ_PHONE_STATE, Permission.CALL_PHONE, Permission.READ_CALL_LOG,
                     Permission.WRITE_CALL_LOG, Permission.ADD_VOICEMAIL, Permission.USE_SIP,
                     Permission.PROCESS_OUTGOING_CALLS, Permission.READ_PHONE_NUMBERS, Permission.ANSWER_PHONE_CALLS};
             } else {
-                PHONE = new String[] {Permission.READ_PHONE_STATE, Permission.CALL_PHONE, Permission.READ_CALL_LOG,
+                PHONE = new String[]{Permission.READ_PHONE_STATE, Permission.CALL_PHONE, Permission.READ_CALL_LOG,
                     Permission.WRITE_CALL_LOG, Permission.ADD_VOICEMAIL, Permission.USE_SIP,
                     Permission.PROCESS_OUTGOING_CALLS};
             }
         }
 
-        public static final String[] SENSORS = new String[] {Permission.BODY_SENSORS};
+        public static final String[] SENSORS = new String[]{Permission.BODY_SENSORS};
 
-        public static final String[] SMS = new String[] {Permission.SEND_SMS, Permission.RECEIVE_SMS,
+        public static final String[] SMS = new String[]{Permission.SEND_SMS, Permission.RECEIVE_SMS,
             Permission.READ_SMS, Permission.RECEIVE_WAP_PUSH, Permission.RECEIVE_MMS};
 
-        public static final String[] STORAGE = new String[] {Permission.READ_EXTERNAL_STORAGE,
+        public static final String[] STORAGE = new String[]{Permission.READ_EXTERNAL_STORAGE,
             Permission.WRITE_EXTERNAL_STORAGE};
     }
 
