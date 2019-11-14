a36f02dc81b0fdcd1e53422270abc8cde34946f8
zhoutingjie <zhoutjcc@msn.cn>
2019-11-14 13:35:23 +0800

    修复StorageReadTest在读取list文件列表时非法字符带来的奔溃

--- permission/src/main/java/com/yanzhenjie/permission/checker/StorageReadTest.java
+++ permission/src/main/java/com/yanzhenjie/permission/checker/StorageReadTest.java
@@ -31,15 +31,17 @@ class StorageReadTest implements PermissionTest {
 
     @Override
     public boolean test() throws Throwable {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !Environment.isExternalStorageLegacy()) return true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !Environment.isExternalStorageLegacy())
+            return true;
 
-        if (!TextUtils.equals(Environment.MEDIA_MOUNTED, Environment.getExternalStorageState())) return true;
+        if (!TextUtils.equals(Environment.MEDIA_MOUNTED, Environment.getExternalStorageState()))
+            return true;
 
         File directory = Environment.getExternalStorageDirectory();
         if (!directory.exists()) return true;
 
         long modified = directory.lastModified();
-        String[] pathList = directory.list();
-        return modified > 0 && pathList != null;
+
+        return modified > 0 && directory.canRead();
     }
 }
